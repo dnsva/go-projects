3b581927250b8aaf082717cc8bbd28c0c5bdac80
Anna <peachtiger0@gmail.com>
2024-07-03 18:21:34 -0400

    basic string to bin/hex working in ncurses

--- text_2_hex/main.go
+++ text_2_hex/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	gc "github.com/gbin/goncurses"
 )
@@ -46,7 +43,7 @@ func hexToStr(hex string) string {
 
 func main() {
 
-	_, err := gc.Init()
+	stdscr, err := gc.Init()
 	if err != nil {
 		log.Fatal("init:", err)
 	}
@@ -57,34 +54,57 @@ func main() {
 	gc.StdScr().Printf("Hello, World!")
 	gc.StdScr().Refresh()
 
+	//char mesg[]="Enter a string: ";		/* message to be appeared on the screen */
+
+	msg := "Enter a string (max size - 30): "
+	//row, col := stdscr.MaxYX()
+	//row, col = (row/2)-1, (col-len(msg))/2
+
+	row, col := 0, 0
+	stdscr.MovePrint(row, col, msg)
+
+	var str string
+	str, err = stdscr.GetString(30) //max 30 chars
+	if err != nil {
+		stdscr.MovePrint(row+1, col, "GetString Error:", err)
+	} else {
+		stdscr.MovePrintf(row+1, col, "You entered: %s", str)
+		//Display binary and hex
+		stdscr.MovePrintf(row+2, col, "Binary: %s", strToBinary(str))
+		stdscr.MovePrintf(row+3, col, "Hex: %s", strToHex(str))
+	}
+
+	stdscr.Refresh()
+
+	stdscr.GetChar()
+
 	/*
-			setlocale(LC_ALL, ""); //enable utf-8
-		    initscr();             //initialize screen
-		    cbreak();              //other ncurses initialization
-		    noecho();              //other ncurses initialization
-		    curs_set(0);           //set cursor pos
-		    keypad(stdscr, TRUE);  //set to standard screen output
-		    start_color();         //enable color
+				setlocale(LC_ALL, ""); //enable utf-8
+			    initscr();             //initialize screen
+			    cbreak();              //other ncurses initialization
+			    noecho();              //other ncurses initialization
+			    curs_set(0);           //set cursor pos
+			    keypad(stdscr, TRUE);  //set to standard screen output
+			    start_color();         //enable color
 
 
-			refresh(); //send everything to screen
+				refresh(); //send everything to screen
 
 
-			endwin(); //end ncurses mode
+				endwin(); //end ncurses mode
 
-			printw("W
+				printw("W
 
-			mvprintw(i+1,0,"%d. %s\n", i+1, options[i].c_str()); //print option
-		            attroff(A_STANDOUT); //unhighlight
-	*/
+				mvprintw(i+1,0,"%d. %s\n", i+1, options[i].c_str()); //print option
+			            attroff(A_STANDOUT); //unhighlight
 
-	reader := bufio.NewReader(os.Stdin) //reader setup
-	text, _ := reader.ReadString('\n')  //read the line from the user
-	text = strings.TrimSpace(text)      //remove whitespace
+		reader := bufio.NewReader(os.Stdin) //reader setup
+		text, _ := reader.ReadString('\n')  //read the line from the user
+		text = strings.TrimSpace(text)      //remove whitespace
 
-	fmt.Println("Binary: ", strToBinary(text))
-	fmt.Println("Hex: ", strToHex(text))
+		fmt.Println("Binary: ", strToBinary(text))
+		fmt.Println("Hex: ", strToHex(text))
 
-	fmt.Println("hello")
+		fmt.Println("hello")*/
 
 }
